6bf8782fae32254a9497d97456bbbfe2c21f801a
Lukas Wagner <lwagner94@posteo.at>
2019-07-31 23:01:59 +0200

    Implement branch operations

--- src/cpu.rs
+++ src/cpu.rs
@@ -9,6 +9,7 @@ pub struct Cpu<'a> {
 }
 
 
+
 impl<'a> Cpu<'a> {
     pub fn new(memory: &'a mut AddressSpace) -> Cpu<'a> {
         Self {
@@ -25,11 +26,22 @@ impl<'a> Cpu<'a> {
         while self.running {
             let encoded_instruction = self.memory.read_word(self.pc);
             let instruction = Instruction::new(encoded_instruction);
+
             self.execute_instruction(instruction);
             self.pc += 4;
+
         }
 
     }
+
+    fn set_pc_for_branch(&mut self, condition: bool, imm: u32) {
+        if condition {
+            let mut new_pc = self.pc as i32;
+            new_pc = new_pc.wrapping_add((imm as i32) * 2);
+            self.pc = new_pc as u32;
+        }
+    }
+
 
     pub fn execute_instruction(&mut self, instruction: Instruction) {
         match instruction {
@@ -42,28 +54,52 @@ impl<'a> Cpu<'a> {
 
             },
             Instruction::JAL(rd, imm) => {
+                let result = self.pc + 4;
 
+                let mut new_pc = self.pc as i32;
+                new_pc = new_pc.wrapping_add((imm as i32) * 2);
+
+                self.pc = new_pc as u32;
+                self.set_register(rd, result);
             },
             Instruction::JALR(rd, rs1, imm) => {
+                let mut new_pc = self.get_register(rs1) as i32;
+                new_pc = new_pc.wrapping_add(imm as i32);
+                let result = self.pc + 4;
+                self.pc = (new_pc as u32) & !1u32;
+                self.set_register(rd, result);
 
             },
-            Instruction::BEQ(r1, rs2, imm) => {
+            Instruction::BEQ(rs1, rs2, imm) => {
+                let v1 = self.get_register(rs1);
+                let v2 = self.get_register(rs2);
+                self.set_pc_for_branch(v1 == v2, imm);
 
             },
-            Instruction::BNE(r1, rs2, imm) => {
-
+            Instruction::BNE(rs1, rs2, imm) => {
+                let v1 = self.get_register(rs1);
+                let v2 = self.get_register(rs2);
+                self.set_pc_for_branch(v1 != v2, imm);
             },
-            Instruction::BLT(r1, rs2, imm) => {
-
+            Instruction::BLT(rs1, rs2, imm) => {
+                let v1 = self.get_register(rs1) as i32;
+                let v2 = self.get_register(rs2) as i32;
+                self.set_pc_for_branch(v1 < v2, imm);
             },
-            Instruction::BGE(r1, rs2, imm) => {
-
+            Instruction::BGE(rs1, rs2, imm) => {
+                let v1 = self.get_register(rs1) as i32;
+                let v2 = self.get_register(rs2) as i32;
+                self.set_pc_for_branch(v1 >= v2, imm);
             },
-            Instruction::BLTU(r1, rs2, imm) => {
-
+            Instruction::BLTU(rs1, rs2, imm) => {
+                let v1 = self.get_register(rs1);
+                let v2 = self.get_register(rs2);
+                self.set_pc_for_branch(v1 < v2, imm);
             },
-            Instruction::BGEU(r1, rs2, imm) => {
-
+            Instruction::BGEU(rs1, rs2, imm) => {
+                let v1 = self.get_register(rs1);
+                let v2 = self.get_register(rs2);
+                self.set_pc_for_branch(v1 >= v2, imm);
             },
             Instruction::LB(rd, rs1, imm) => {
 
@@ -259,6 +295,100 @@ mod test {
         register_test!(SRA, 0b1000_0000_0000_0000_0000_0000_0000_0010, 1, 0b1100_0000_0000_0000_0000_0000_0000_0001);
 
     }
+
+    #[test]
+    fn test_jal() {
+        fn t(offset: i32) {
+            let mut memory = AddressSpace::new();
+            let mut cpu = Cpu::new(&mut memory);
+            cpu.pc = 80;
+
+            cpu.execute_instruction(Instruction::JAL(1, offset as u32));
+            assert_eq!(cpu.get_register(1), 84);
+            assert_eq!(cpu.pc, (80 + offset * 2) as u32);
+        }
+
+        t(16);
+        t(-16);
+
+    }
+
+    #[test]
+    fn test_jalr() {
+        fn t(base: u32, offset: i32) {
+            let mut memory = AddressSpace::new();
+            let mut cpu = Cpu::new(&mut memory);
+            cpu.set_register(2, base);
+            cpu.pc = 80;
+
+            cpu.execute_instruction(Instruction::JALR(1, 2, offset as u32));
+            assert_eq!(cpu.get_register(1), 84);
+            assert_eq!(cpu.pc, ((base as i32 + offset) as u32) & !1u32);
+        }
+
+        t(400, 4);
+        t(400, -4);
+        t(400, 1);
+        t(400, -1);
+
+    }
+
+    macro_rules! branch_test {
+        ($instr:ident, $first:expr, $second:expr, $offset:expr, $expect_jump:expr) => {
+            {
+                let first = $first as u32;
+                let second = $second as u32;
+
+                let mut memory = AddressSpace::new();
+                let mut cpu = Cpu::new(&mut memory);
+                cpu.set_register(2, first);
+                cpu.set_register(3, second);
+                cpu.pc = 80;
+
+                cpu.execute_instruction(Instruction::$instr(2, 3, $offset));
+
+                if $expect_jump {
+                    assert_eq!((80i32).wrapping_add(2 * $offset) as u32, cpu.pc);
+                } else {
+                    assert_eq!(cpu.pc, 80);
+                }
+            }
+        };
+    }
+
+    #[test]
+    fn test_branching() {
+        branch_test!(BEQ, 10, 10, 8, true);
+        branch_test!(BEQ, -10i32, -10i32, 8, true);
+        branch_test!(BEQ, 10, 9, 8, false);
+        branch_test!(BEQ, -10i32, -9i32, 8, false);
+
+        branch_test!(BNE, 10, 10, 8, false);
+        branch_test!(BNE, -10i32, -10i32, 8, false);
+        branch_test!(BNE, 10, 9, 8, true);
+        branch_test!(BNE, -10i32, -9i32 as u32, 8, true);
+
+        branch_test!(BLT, 10, 10, 8, false);
+        branch_test!(BLT, 11, 10, 8, false);
+        branch_test!(BLT, 9, 10, 8, true);
+        branch_test!(BLT, -9i32, 10, 8, true);
+
+        branch_test!(BGE, 10, 10, 8, true);
+        branch_test!(BGE, 11, 10, 8, true);
+        branch_test!(BGE, 9, 10, 8, false);
+        branch_test!(BGE, -9i32, 10, 8, false);
+
+        branch_test!(BLTU, -10i32, 10, 8, false);
+        branch_test!(BLTU, 9, 10, 8, true);
+        branch_test!(BLTU, 10, 10, 8, false);
+        branch_test!(BLTU, -10i32, -9i32, 8, true);
+
+        branch_test!(BGEU, -10i32, 10, 8, true);
+        branch_test!(BGEU, 9, 10, 8, false);
+        branch_test!(BGEU, 10, 10, 8, true);
+        branch_test!(BGEU, -10i32, -9i32, 8, false);
+    }
+
 
     #[test]
     fn test_register() {
