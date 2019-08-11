46fbc2796bdd802ac4951137d2c0253d08634c39
Lukas Wagner <lwagner94@posteo.at>
2019-08-11 22:27:01 +0200

    Improve framebuffer performance

--- src/memory/video.rs
+++ src/memory/video.rs
@@ -2,22 +2,22 @@ use super::addressspace::Address;
 use super::addressspace::MemoryDevice;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::pixels::{Color, PixelFormat, PixelFormatEnum};
-use sdl2::render::{Canvas, Texture};
-use sdl2::video::Window;
+use sdl2::pixels::{Color, PixelFormatEnum};
 use sdl2::rect::Rect;
+use sdl2::render::Texture;
 use std::process;
+use std::ptr::copy_nonoverlapping;
 use std::thread;
 use std::time::Duration;
 
-use std::sync::{Arc, Mutex};
-
 const SIZE_X: u32 = 800;
 const SIZE_Y: u32 = 600;
+const VEC_SIZE: usize = (SIZE_X * SIZE_Y * 3) as usize;
+
+static mut FRAMEBUFFER: [u8; VEC_SIZE] = [0; VEC_SIZE];
 
 pub struct Video {
     offset: Address,
-    framebuffer: Arc<Mutex<Vec<u8>>>
 }
 
 impl MemoryDevice for Video {
@@ -32,10 +32,11 @@ impl MemoryDevice for Video {
     }
 
     fn write_byte(&mut self, _address: Address, _val: u8) {
-        let mut framebuffer = self.framebuffer.lock().unwrap();
         let relative_address = self.get_relative_address(_address) as usize;
-
-        framebuffer[relative_address] = _val;
+        //        self.framebuffer[relative_address] = _val;
+        unsafe {
+            FRAMEBUFFER[relative_address] = _val;
+        }
     }
     fn write_halfword(&mut self, _address: Address, _val: u16) {
         unimplemented!();
@@ -51,12 +52,6 @@ impl MemoryDevice for Video {
 
 impl Video {
     pub fn new(offset: Address) -> Video {
-        const VEC_SIZE: u32 = SIZE_X * SIZE_Y * 3;
-
-        let data = Arc::new(Mutex::new(vec![0u8; VEC_SIZE as usize]));
-
-        let data_clone = Arc::clone(&data);
-
         thread::spawn(move || {
             let sdl_context = sdl2::init().unwrap();
             let video_subsystem = sdl_context.video().unwrap();
@@ -75,12 +70,13 @@ impl Video {
 
             let texture_creator = canvas.texture_creator();
 
-            let mut texture: Texture = texture_creator.create_texture_streaming(
-                Some(PixelFormatEnum::RGB24), SIZE_X, SIZE_Y).unwrap();
+            let mut texture: Texture = texture_creator
+                .create_texture_streaming(Some(PixelFormatEnum::RGB24), SIZE_X, SIZE_Y)
+                .unwrap();
 
             let mut event_pump = sdl_context.event_pump().unwrap();
-            let mut i = 0;
-            'running: loop {
+
+            loop {
                 for event in event_pump.poll_iter() {
                     match event {
                         Event::Quit { .. }
@@ -93,29 +89,25 @@ impl Video {
                         _ => {}
                     }
                 }
-                // The rest of the game loop goes here...
-                let data = data_clone.lock().unwrap();
 
-
-
-                texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
-                    buffer.copy_from_slice(data.as_slice());
-
-                }).unwrap();
+                texture
+                    .with_lock(None, |buffer: &mut [u8], _pitch: usize| {
+                        let dest_ptr = buffer.as_mut_ptr();
+                        unsafe {
+                            copy_nonoverlapping(FRAMEBUFFER.as_ptr(), dest_ptr, VEC_SIZE);
+                        }
+                    })
+                    .unwrap();
                 canvas.clear();
-                canvas.copy(&texture, None, Some(Rect::new(0, 0, SIZE_X, SIZE_Y))).unwrap();
-
-
-
+                canvas
+                    .copy(&texture, None, Some(Rect::new(0, 0, SIZE_X, SIZE_Y)))
+                    .unwrap();
 
                 canvas.present();
-                ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+                thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
             }
         });
 
-        Video {
-            offset,
-            framebuffer: data
-        }
+        Video { offset }
     }
 }
